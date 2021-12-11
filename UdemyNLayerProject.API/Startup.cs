fa9cec4b33bf6a0ce1f8e51646c16f0d78f54ba5
Berkay Yalçın <brky.yalcin@hotmail.com>
2021-12-11 13:49:38 +0300

    Initial commit

---
+++ UdemyNLayerProject.API/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.UnitOfWorks;
+using UdemyNLayerProject.Data;
+using UdemyNLayerProject.Data.UnitOfWorks;
+
+namespace UdemyNLayerProject.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseNpgsql(Configuration["ConnectionStrings:ConnectionStrings"].ToString(),o =>
+                {
+                    o.MigrationsAssembly("UdemyNLayerProject.Data");
+                });
+            });
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
